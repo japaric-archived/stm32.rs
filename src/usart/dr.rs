78259a4757a7fbb11bbb2101e314b73d6dd32c2e
Jorge Aparicio <japaricious@gmail.com>
2015-03-27 23:32:09 -0500

    feat(usart): expose the contents of the DR register

--- src/usart/dr.rs
+++ src/usart/dr.rs
@@ -4,6 +4,13 @@ use volatile::Into;
 #[repr(C)]
 pub struct Register(u32);
 
+impl Register {
+    /// Returns the lower byte of the register
+    pub fn u8(&self) -> u8 {
+        self.0 as u8
+    }
+}
+
 impl Into<Register> for u8 {
     fn convert_into(self) -> Register {
         Register(self as u32)
